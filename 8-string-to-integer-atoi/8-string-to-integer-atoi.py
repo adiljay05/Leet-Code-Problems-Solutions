b29168d991d10529874ce402f2821d9bba8f0e03
Jawad Adil <51061044+adiljay05@users.noreply.github.com>
2022-03-01 21:58:21 +0000

    Time: 39 ms (76.05%), Space: 14 MB (60.25%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -0,0 +1,36 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.strip()
+        if s=="":
+            return 0
+        s = s.split(' ')[0]
+        new_s = ""
+        neg = False
+        pos = False
+        if s[0]=='-' or s[0]=='+' or s[0]>='0' and s[0]<='9':
+            if s[0]=='-':
+                neg = True
+            if s[0]=='+':
+                pos = True
+            i = 0
+            for s1 in s:
+                if neg and i==0 or pos and i==0:
+                    i+=1
+                    continue
+                if s1>='0' and s1<='9':
+                    new_s+=s1
+                else:
+                    break
+        if neg:
+            new_s = "-"+new_s
+        print(new_s)
+        try:
+            i = int(float(new_s))
+            if i < -2147483648:
+                return -2147483648
+            if i > 2147483647:
+                return 2147483647
+            return i
+        except:
+            return 0
+        
